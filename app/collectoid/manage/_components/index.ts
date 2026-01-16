d1810591c19a33b29797eb7bad7fc0d447acabe3
Keith Hayes <kwnt592@astrazeneca.net>
2026-01-16 10:32:19 +0000

    removed management v2 screens

--- app/collectoid/manage/_components/index.ts
+++
@@ -1,5 +0,0 @@
-export { CollectionProvider, useCollection } from "./collection-context"
-export { TabNavigation } from "./tab-navigation"
-export { StateBadge } from "./state-badge"
-export { CollectionHeader } from "./collection-header"
-export { TabContent } from "./tab-content"
